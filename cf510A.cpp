c62c84bbf444146f9fb5a3cdaab723d4990b102e
Mehta-Anmol <anmolmehta01121978@gmail.com>
2026-01-19 08:24:58 +0530

    19-01-2026

---
+++ cf510A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,m;
+    cin>>n>>m;
+    bool first = false;
+    for(int i=0;i<n;i++){
+        if(i%2 == 0){
+            for(int j=0;j<m;j++){
+                cout<<'#';
+            }
+        }
+        else{
+            if(first){
+                cout<<'#';
+            }
+            else{
+                cout<<'.';
+            }
+            for(int j=1;j<m-1;j++){
+                cout<<'.';
+            }
+            if(first){
+                cout<<'.';
+            }
+            else{
+                cout<<'#';
+            }
+            first = !first;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
