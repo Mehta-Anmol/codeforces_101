dc6d67e637685b5a9287111710992ea9efc3df22
Mehta-Anmol <anmolmehta01121978@gmail.com>
2026-01-25 18:54:08 +0530

    25-01-2026

---
+++ cf1877A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        long long sum = 0;
+        for(int i=0;i<n-1;i++){
+            int num;
+            cin>>num;
+            sum+= num;
+        }
+        long long ans = 0-sum;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
