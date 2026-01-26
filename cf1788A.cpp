b762aaa85b2f1f070e1f68aa2a3c1239e5e090ec
Mehta-Anmol <anmolmehta01121978@gmail.com>
2026-01-26 15:36:53 +0530

    26-01-2026

---
+++ cf1788A.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> nums(n);
+        int total = 0;
+        for(int i=0;i<n;i++){
+            cin>>nums[i];
+            if(nums[i] == 2){
+                total++;
+            }
+        }
+        if(total%2 != 0){
+            cout<<-1<<endl;
+        }
+        else if(total == 0){
+            cout<<1<<endl;
+        }
+        else{
+            int target = total/2;
+            for(int i=0;i<n;i++){
+                if(nums[i] == 2){
+                    target--;
+                }
+                if(target == 0){
+                    cout<<i+1<<endl;
+                    break;
+                }
+            }
+
+        }
+
+
+    }
+    return 0;
+}
