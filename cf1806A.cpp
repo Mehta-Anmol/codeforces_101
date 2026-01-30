3420961f30deb743bed1f659b928c87cc5ad1fd1
Mehta-Anmol <anmolmehta01121978@gmail.com>
2026-01-30 17:22:47 +0530

    30-01-2026

---
+++ cf1806A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+
+        if(b > d || (c-a > d-b)){
+            cout<<-1<<endl;
+        }
+        else{
+            cout<<(d-b)+(a-c+d-b)<<endl;
+        }
+    }
+    return 0;
+}
