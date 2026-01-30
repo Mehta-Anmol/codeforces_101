cb09d11b5fd1387ecdb312fba38c26cbad7e5610
Mehta-Anmol <anmolmehta01121978@gmail.com>
2026-01-30 17:37:28 +0530

    30-01-2026

---
+++ cf1831A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> nums(n);
+        for(int i=0;i<n;i++){
+            cin>>nums[i];
+        }
+        
+        int sum = n+1;
+        for(int i: nums){
+            cout<<sum-i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
