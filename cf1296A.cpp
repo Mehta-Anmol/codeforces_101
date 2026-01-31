b198d96e447ebf96d92267cd10bc364c901de579
Mehta-Anmol <anmolmehta01121978@gmail.com>
2026-01-31 17:55:39 +0530

    31-01-2026

---
+++ cf1296A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int odd = 0;
+        int even = 0;
+        for(int i=0;i<n;i++){
+            int num;
+            cin>>num;
+
+            if(num%2 != 0){
+                odd++;
+            }
+            else{
+                even++;
+            }
+        }
+        if(odd%2 != 0){
+            cout<<"YES"<<endl;
+        }
+        else if(even > 0 && odd > 0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+        
+    }
+    return 0;
+}
