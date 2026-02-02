3a0d0e98a1aa78d3bb0f5304a2184828c44b19ca
Mehta-Anmol <anmolmehta01121978@gmail.com>
2026-02-03 02:44:54 +0530

    03-02-2026

---
+++ cf50A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a,b;
+    cin>>a>>b;
+    long long mul = a*b;
+    long long ans = mul/2;
+    cout<<ans<<endl;
+    
+    return 0;
+}
