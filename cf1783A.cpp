818837c970ee364c5bf5255b8c270babbb57224c
Mehta-Anmol <anmolmehta01121978@gmail.com>
2026-02-16 12:25:45 +0530

    16-02-2026

---
+++ cf1783A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> nums(n);
+        for(int i=0;i<n;i++){
+            cin>>nums[i];
+        }
+        sort(nums.begin(),nums.end());
+        if(nums[0] == nums[n-1]){
+            cout<<"NO"<<endl;
+            continue;
+        }
+        cout<<"YES"<<endl;
+        cout<<nums[n-1]<<" ";
+        for(int i=0;i<n-1;i++){
+            cout<<nums[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
