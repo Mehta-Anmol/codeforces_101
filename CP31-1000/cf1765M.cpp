fe3989aae7752e0e5086613f645a5e62067afac4
Mehta-Anmol <anmolmehta01121978@gmail.com>
2026-04-16 17:26:25 +0530

    minimum LCM

---
+++ CP31-1000/cf1765M.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    ll t;
+    cin>>t;
+    
+    while(t--){
+        ll n;
+        cin>>n;
+        ll a = 1;
+        ll b = n-1;
+        for(ll i=2;i*i<=n;i++){
+            if(n%i == 0){
+                a = n/i;
+                b = n-a;
+                break;
+            }
+        }
+        
+        cout<<a<<" "<<b<<endl;
+    }
+    return 0;
+}
